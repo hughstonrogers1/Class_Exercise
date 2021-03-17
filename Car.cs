7d0e41ff7b0ba1c2657c5ed461579a90b32c1fc9
hughstonrogers1 <hughstonrogers1>
2021-03-16 20:50:18 -0400

    Class_Exercise

---
+++ Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Exercise
+{
+    public class Car
+    {
+        //public Car()
+        //{
+
+        //}
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public string ShowCarDetails()
+        {
+            return $"Make: {Make}\n" +
+                    $"Model: {Model}\n" +
+                    $"Year: {Year}\n";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class_Exercise
+{
+    public class Program
+    {
+        public string MyField;
+
+        internal int MyProperty { get; set; }
+
+
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+
+            Car c1 = new Car("Toyota", "4Runner", 2021);
+
+
+            Console.WriteLine(c1.ShowCarDetails());
+
+
+
+
+
+        }
+    }
+}
